0ed01e9860e01b0a1dfe63c3e88bb38dacaa2dae
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 17:47:24 -0300

    Centraliza configuración de invitación Plim Plim

---
+++ js/config.js
@@ -0,0 +1,47 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Alessandro",
+    ageLabel: "CUMPLE 1 AÑITO",
+    message: "Celebremos juntos mi primer cumpleaños.",
+    eventDateTime: "2026-10-09T16:00:00-03:00",
+    eventEndDateTime: "2026-10-09T19:00:00-03:00",
+    dateLabel: "Viernes 9 de Octubre",
+    timeLabel: "16:00 a 19:00 hs.",
+    venue: "Salón de Fiestas Cirilo",
+    address: "Av. Álvarez Thomas 1320, CABA",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=Sal%C3%B3n+de+Fiestas+Cirilo+Av.+%C3%81lvarez+Thomas+1320+CABA",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Alessandro. 🎈🎉",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio",
+    audioUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionPlimPlim/main/www.tarjetas.cool/modelos/cumple/plim_plim/sounds/audio3860.mp3",
+    portraitUrl: "https://media1.giphy.com/media/C2TcUtEGtMtz0fxHQK/giphy.gif?cid=6c09b95200c6185f11eebba62dbf3ffdf8456d5ba8106ffd&rid=giphy.gif&ct=s",
+    ribbonUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionPlimPlim/main/www.tarjetas.cool/modelos/cumple/plim_plim/img/cinta.png"
+  },
+  characters: [
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionPlimPlim/main/www.tarjetas.cool/modelos/cumple/plim_plim/img/1.png",
+      width: "min(108vw, 490px)",
+      top: "-44px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionPlimPlim/main/www.tarjetas.cool/modelos/cumple/plim_plim/img/4.png",
+      width: "min(122vw, 540px)",
+      top: "-96px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionPlimPlim/main/www.tarjetas.cool/modelos/cumple/plim_plim/img/3.png",
+      width: "min(114vw, 510px)",
+      top: "-45px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionPlimPlim/main/www.tarjetas.cool/modelos/cumple/plim_plim/img/2.png",
+      width: "min(90vw, 400px)",
+      top: "-72px",
+      translateX: "-49%"
+    }
+  ]
+};
